63aa50698627c6436e9bd85f55c761fda78e3616
kevinkeyssx <keyss1700@gmail.com>
2026-05-04 00:14:49 -0400

    Layout added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import QueryProvider from '../components/QueryProvider.svelte';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+
+	let { children } = $props();
+</script>
+
+<QueryProvider>
+	{@render children()}
+	<SvelteQueryDevtools />
+</QueryProvider>
