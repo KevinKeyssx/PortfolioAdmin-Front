b45e07562c7a31f7a14f8e793f4a23cbaf3b9a58
kevinkeyssx <keyss1700@gmail.com>
2026-07-07 21:32:54 -0400

    Styles updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import '../app.css';
-	import QueryProvider from '../components/QueryProvider.svelte';
-	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+    import '../app.css';
+    import QueryProvider            from '../components/QueryProvider.svelte';
+    import { SvelteQueryDevtools }  from '@tanstack/svelte-query-devtools';
 
-	let { children } = $props();
+
+    let { children } = $props();
 </script>
 
+
 <QueryProvider>
-	{@render children()}
-	<SvelteQueryDevtools />
+    {@render children()}
+    <SvelteQueryDevtools />
 </QueryProvider>
